0fba62e745f04b0c53f93e893944ba3f4b051eb8
carlos-palalo <palaxcarlos@gmail.com>
2020-12-06 22:49:01 +0100

    fadf

--- admin/index.php
+++ admin/index.php
@@ -5,16 +5,73 @@ try {
     session_start();
     require "../conexion.php";
     $tablas = $bd->prepare('SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = "proyectotiendaropa_carlosp"');
-    $columnas = $bd->prepare('SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = "cesta"');
     $nombreTabla = "cesta";
     $info =  $bd->prepare('SELECT * FROM ' . $nombreTabla . '', array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
     if (isset($_REQUEST['tabla'])) {
         $nombreTabla = $_REQUEST['tabla'];
-        $columnas = $bd->prepare('SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = "' . $nombreTabla . '"');
         $info =  $bd->prepare('SELECT * FROM ' . $nombreTabla . '', array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
     }
-} catch (Exception $e) {
-    echo "<script>console.log('" . $e . "')</script>";
+    if (isset($_REQUEST['btn-ordenar'])) {
+        $nombreTabla = $_REQUEST['tabla_name'];
+        if ($_REQUEST['tipo_orden'] != "") {
+            $info =  $bd->prepare('SELECT * FROM ' . $nombreTabla . ' ORDER BY ' . $_REQUEST['orden'] . ' ' . $_REQUEST['tipo_orden'] . '', array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+        } else {
+            $info =  $bd->prepare('SELECT * FROM ' . $nombreTabla . '', array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+        }
+    }
+    if (isset($_REQUEST['btn-buscar'])) {
+        $nombreTabla = $_REQUEST['tabla_name'];
+        if ($_REQUEST['campo'] != "" && $_REQUEST['columna'] != "") {
+            $campo = htmlspecialchars($_REQUEST['campo']);
+            $info =  $bd->prepare('SELECT * FROM ' . $nombreTabla . ' WHERE ' . $_REQUEST['columna'] . ' = "' . $campo . '"', array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+        } else {
+            $info =  $bd->prepare('SELECT * FROM ' . $nombreTabla . '', array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+        }
+    }
+    if (isset($_REQUEST['btn-actualizar'])) {
+        $nombreTabla = $_REQUEST['tabla_name'];
+        $columnas = $bd->prepare('SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = "' . $nombreTabla . '"');
+        $flag = false;
+        if (strcasecmp($nombreTabla, "cesta") == 0 || strcasecmp($nombreTabla, "pedido") == 0) {
+            echo "<script>alert('No puedes modificar la tabla " . strtoupper($nombreTabla) . "');</script>";
+        } else {
+            for ($i = 1; $i <= $_REQUEST['numFilas']; $i++) {
+                $cont = 0;
+                $cad = 'UPDATE ' . $nombreTabla . ' SET ';
+                $columnas->execute();
+                $fila = $columnas->fetch(PDO::FETCH_OBJ);
+                $id = $fila->COLUMN_NAME;
+                while ($fila = $columnas->fetch(PDO::FETCH_OBJ)) {
+                    $request = $fila->COLUMN_NAME . $i;
+                    if ($_REQUEST[$request] != "") {
+                        if ($cont == 0) {
+                            $cad .= $fila->COLUMN_NAME . " = '" . $_REQUEST[$request] . "'";
+                            $cont++;
+                        } else {
+                            $cad .= ", " . $fila->COLUMN_NAME . " = '" . $_REQUEST[$request] . "'";
+                        }
+                    }
+                }
+                $request = $id . $i;
+                $cad .= " WHERE " . $id . " = '" . $_REQUEST[$request] . "'";
+                $actu = $bd->prepare($cad);
+                $actu->execute();
+                if ($actu->rowCount() != 0) {
+                    $flag = true;
+                }
+            }
+
+            if ($flag == true) {
+                echo "<script>alert('Datos actualizados con éxito');</script>";
+            } else {
+                echo "<script>alert('No se ha modificado ningún dato');</script>";
+            }
+        }
+        $info =  $bd->prepare('SELECT * FROM ' . $nombreTabla . '', array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+    }
+    $columnas = $bd->prepare('SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = "' . $nombreTabla . '"');
+} catch (PDOException $e) {
+    echo "Error - " . $e->getMessage();
 }
 ?>
 
@@ -37,6 +94,10 @@ try {
             padding: 0px;
         }
 
+        .row {
+            margin-bottom: 0px !important;
+        }
+
         .top-nav {
             padding-right: 300px;
         }
@@ -56,6 +117,29 @@ try {
             color: black;
             font-size: medium;
             padding-left: 10px;
+        }
+
+        .filtro button {
+            padding: 3px 0px;
+            width: 150px;
+        }
+
+        .filtro button:nth-child(2) {
+            float: right;
+        }
+
+        table {
+            margin: 0 auto;
+            width: fit-content;
+        }
+
+        table td {
+            padding: 0px;
+            width: fit-content;
+        }
+
+        table input {
+            text-align: center;
         }
 
         .logo {
@@ -137,8 +221,23 @@ try {
             font-weight: bold;
         }
 
-        table td {
-            padding: 15px 0px;
+        form button:hover {
+            background-color: black !important;
+            color: white !important;
+            opacity: 0.9;
+        }
+
+        .busqueda {
+            font-size: large;
+            font-weight: bold;
+            text-align: center;
+            padding-top: 50px;
+            height: 150px;
+            width: 100%;
+            left: 150px;
+            position: fixed;
+            background-color: white;
+            z-index: 1;
         }
     </style>
     <script>
@@ -176,97 +275,135 @@ try {
                     while ($fila = $tablas->fetch(PDO::FETCH_OBJ)) {
                         echo "<li class='f pantalones'><input type='submit' name='tabla' value='" . strtoupper($fila->TABLE_NAME) . "'></li>";
                     }
-                } catch (Exception $e) {
-                    echo "<script>console.log('Error Categorías. " . $e . "')</script>";
+                } catch (PDOException $e) {
+                    echo "Error - " . $e->getMessage();
                 }
                 ?>
             </form>
         </ul>
     </header>
     <main>
-        <div class="container-filtro">
-            <div class="container">
-                <div class="row">
-                    <div class="filtro col s12 m4">
-                        <label>Ordenar</label>
-                        <select class="browser-default">
-                            <option value="" selected></option>
-                            <option value="1">De mayor a menor (&gt;)</option>
-                            <option value="2">De menor a mayor (&lt;)</option>
-                        </select>
+        <form method="POST">
+
+            <div class="container-filtro">
+                <div class="container">
+                    <div class="row">
+                        <?php
+                        echo '<input type="hidden" name="tabla_name" value="' . $nombreTabla . '">';
+                        ?>
+                        <div class="filtro col s12 m4">
+                            <select name="orden" class="browser-default">
+                                <option value="" selected>Ordenar por...</option>
+                                <?php
+                                try {
+                                    $columnas->execute();
+                                    while ($fila = $columnas->fetch(PDO::FETCH_OBJ)) {
+                                        echo "<option value='" . $fila->COLUMN_NAME . "'>" . $fila->COLUMN_NAME . "</option>";
+                                    }
+                                } catch (PDOException $e) {
+                                    echo "Error - " . $e->getMessage();
+                                }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="filtro col s12 m4">
+                            <select name="tipo_orden" class="browser-default">
+                                <option value="">Tipo</option>
+                                <option value="ASC">Ascendente</option>
+                                <option value="DESC">Descendente</option>
+                            </select>
+                        </div>
+                        <div class="filtro col s12 m4">
+                            <button class="btn white black-text lighten-1" type="submit" name="btn-ordenar">Ordenar</button>
+                            <button class="btn white black-text lighten-1" type="submit" name="btn-actualizar">Actualizar</button>
+                        </div>
                     </div>
-                    <div class="filtro col s12 m3">
-                        <label>Sexo</label>
-                        <select class="browser-default">
-                            <option value="" selected></option>
-                            <option value="1">Hombre</option>
-                            <option value="2">Mujer</option>
-                        </select>
+                    <div class="row">
+                        <?php
+                        echo '<input type="hidden" name="tabla_name" value="' . $nombreTabla . '">';
+                        ?>
+                        <div class="filtro col s12 m4">
+                            <select name="columna" class="browser-default">
+                                <option value="" selected>Buscar por...</option>
+                                <?php
+                                try {
+                                    $columnas->execute();
+                                    while ($fila = $columnas->fetch(PDO::FETCH_OBJ)) {
+                                        echo "<option value='" . $fila->COLUMN_NAME . "'>" . $fila->COLUMN_NAME . "</option>";
+                                    }
+                                } catch (PDOException $e) {
+                                    echo "Error - " . $e->getMessage();
+                                }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="filtro col s12 m4">
+                            <input type="text" name="campo" placeholder="Introduce el valor">
+                        </div>
+                        <div class="filtro col s12 m4">
+                            <button class="btn white black-text lighten-1" type="submit" name="btn-buscar">Buscar</button>
+                            <button class="btn white black-text lighten-1" type="submit" name="btn-eliminar" disabled>Eliminar</button>
+                        </div>
                     </div>
-                    <div class="filtro col s12 m2">
-                        <label>Talla</label>
-                        <select class="browser-default">
-                            <option value="" selected></option>
-                            <option value="1">XS</option>
-                            <option value="2">S</option>
-                            <option value="3">M</option>
-                            <option value="1">XL</option>
-                            <option value="2">XXL</option>
-                            <option value="3">3XL</option>
-                            <option value="3">4XL</option>
-                        </select>
-                    </div>
-                    <div class="filtro col s12 m3">
-                        <label>Marca</label>
-                        <select class="browser-default">
-                            <option value="" selected></option>
-                            <option value="1">Pier One</option>
-                            <option value="2">Adidas</option>
-                            <option value="3">Decathlon</option>
-                        </select>
-                    </div>
+
+                </div>
+                <!-- Ordenar precio, talla, marca  -->
+            </div>
+            <div class="row">
+                <div class="tabla col s12 m12">
+                    <form id="tabla" method="POST" name="<?php echo $nombreTabla; ?>">
+                        <table class="highlight centered">
+                            <thead>
+                                <?php
+                                echo "<tr>";
+                                try {
+                                    $i = 0;
+                                    $columnas->execute();
+                                    while ($fila = $columnas->fetch(PDO::FETCH_OBJ)) {
+                                        $column[$i] = $fila->COLUMN_NAME;
+                                        echo "<th>" . $column[$i] . "</th>";
+                                        $i++;
+                                    }
+                                } catch (PDOException $e) {
+                                    echo "Error - " . $e->getMessage();
+                                }
+                                echo "</tr>";
+                                ?>
+                            </thead>
+                            <tbody>
+                                <?php
+                                try {
+                                    $numFilas = 0;
+                                    $info->execute();
+                                    if ($info->rowCount() != 0) {
+                                        while ($fila = $info->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+                                            $i = 0;
+                                            $numFilas++;
+                                            echo "<tr>";
+                                            foreach ($fila as $value) {
+                                                echo "<td><input type='text' name='" . $column[$i] . "" . $numFilas . "' value='" . $value . "'></td>";
+                                                $i++;
+                                            }
+                                            echo "</tr>";
+                                        }
+                                        echo '<input type="hidden" name="numFilas" value="' . $numFilas . '">';
+                                    } else {
+                                        echo "<div class='busqueda'>Lo siento, no se han encontrado resultados acordes con la búsqueda</div>";
+                                    }
+                                } catch (PDOException $e) {
+                                    echo "Error - " . $e->getMessage();
+                                }
+                                ?>
+                            </tbody>
+                            <?php
+
+                            ?>
+                        </table>
+                    </form>
                 </div>
             </div>
-            <!-- Ordenar precio, talla, marca  -->
-        </div>
-        <div class="row">
-            <div class="tabla col s12 m12">
-                <form id="tabla" method="POST" name="<?php echo $nombreTabla; ?>">
-                    <table class="highlight centered">
-                        <thead>
-                            <?php
-                            echo "<tr>";
-                            try {
-                                $columnas->execute();
-                                while ($fila = $columnas->fetch(PDO::FETCH_OBJ)) {
-                                    echo "<th>" . $fila->COLUMN_NAME . "</th>";
-                                }
-                            } catch (Exception $e) {
-                                echo "<script>console.log('Error Categorías. " . $e . "')</script>";
-                            }
-                            echo "</tr>";
-                            ?>
-                        </thead>
-                        <tbody>
-                            <?php
-                            try {
-                                $info->execute();
-                                while ($fila = $info->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
-                                    echo "<tr>";
-                                    foreach ($fila as $value) {
-                                        echo "<td contenteditable='true'>" . $value . "</td>";
-                                    }
-                                    echo "</tr>";
-                                }
-                            } catch (Exception $e) {
-                                echo "<script>console.log('Error Categorías. " . $e . "')</script>";
-                            }
-                            ?>
-                        </tbody>
-                    </table>
-                </form>
-            </div>
-        </div>
+        </form>
+
     </main>
 </body>
 
